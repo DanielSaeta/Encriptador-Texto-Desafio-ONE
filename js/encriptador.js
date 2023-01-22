5f3db31ad2c1e0ff82bd618f536f2c11800879f5
DanielSaeta <daniel_saeteros@outlook.com>
2023-01-22 18:08:17 -0500

    Variables principales y función base para el botón Encriptar + ocultar/mostrar la instrucción/mensaje de resultado

--- js/encriptador.js
+++ js/encriptador.js
@@ -1,1 +1,15 @@
-alert("JS conectado");
+var inputText = document.querySelector(".input-text");
+var outputText = document.querySelector(".output-text");
+var instruccion = document.querySelector(".instruccion");
+var mensaje = document.querySelector(".mensaje");
+
+function resultadoEncriptar(){
+    var textoIngresado = encriptar(inputText.value);
+    console.log(textoIngresado);
+    instruccion.style.display = "none";
+    mensaje.style.display = "inline-flex";
+} 
+
+function encriptar(textoCapturado){
+    return textoCapturado;
+}
