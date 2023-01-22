2c35c2cc568fea1528d547165fe2fbecd4425e56
DanielSaeta <daniel_saeteros@outlook.com>
2023-01-22 18:55:30 -0500

    Función encriptar convierte siempre el texto ingresado a minúsculas

--- js/encriptador.js
+++ js/encriptador.js
@@ -5,13 +5,20 @@ var mensaje = document.querySelector(".mensaje");
 
 function resultadoEncriptar(){
     var textoIngresado = encriptar(inputText.value);
+
     //muestra el mismo texto ingresado en el textarea
     outputText.value = textoIngresado;
-    //oculta la instrucción y muestra el resultado
+    //oculta la instrucción y 
     instruccion.style.display = "none";
+    //muestra el resultado
     mensaje.style.display = "inline-flex";
 } 
 
 function encriptar(textoCapturado){
+    var llavesEncriptacion = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
+
+    //convierte a minúsculas
+    textoCapturado = textoCapturado.toLowerCase();
+
     return textoCapturado;
 }
