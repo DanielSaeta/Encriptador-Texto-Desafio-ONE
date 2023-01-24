3027eb7fb74da9a1302a50608eb786df344b6c56
DanielSaeta <daniel_saeteros@outlook.com>
2023-01-24 12:32:37 -0500

    Corrección de las funciones resultadoEncriptar y resultadoDesencriptar para que mantengan visible la instrucción si el campo principal esta vacío

--- js/encriptador.js
+++ js/encriptador.js
@@ -4,37 +4,45 @@ var instruccion = document.querySelector(".instruccion");
 var mensaje = document.querySelector(".mensaje");
 
 //Validar acentos, números y mayúsculas
-function validacion(e){
+function validacion(e) {
     var key = e.keyCode || e.which; 
     var tecla = String.fromCharCode(key).toString();
     var letrasPermitidas = " abcdefghijklmnñopqrstuvwxyz";
      
-    if(letrasPermitidas.indexOf(tecla) == -1 ){
-        alert ("ingresa solo letras minúsculas sin acentos y sin números");
+    if (letrasPermitidas.indexOf(tecla) == -1) {
+        alert ("Ingresa solo letras minúsculas. No se aceptan acentos, caracteres especiales y números");
         return false;
     }
 }
 
 //Encriptación
-function resultadoEncriptar(){
+function resultadoEncriptar() {
     var textoEncriptado = encriptar(inputText.value);
 
-    //muestra el resultado del texto ya encriptado
-    outputText.value = textoEncriptado;
-    //vaciar textarea del bloque izquierdo
-    inputText.value = "";
-    //oculta la instrucción y 
-    instruccion.style.display = "none";
-    //muestra el resultado
-    mensaje.style.display = "inline-flex";
-} 
+    if(inputText.value.length == 0) {
+        //muestra la instrucción 
+        instruccion.style.display = "inline-flex";
+        //oculta el resultado
+        mensaje.style.display = "none";
+        inputText.focus();
+    } else {
+        //muestra el resultado del texto ya encriptado
+        outputText.value = textoEncriptado;
+        //vaciar textarea del bloque izquierdo
+        inputText.value = "";
+        //oculta la instrucción 
+        instruccion.style.display = "none";
+        //muestra el resultado
+        mensaje.style.display = "inline-flex";
+    }
+}
 
-function encriptar(textoCapturado){
+function encriptar(textoCapturado) {
     let llavesEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
 
     //encriptación
-    for(i = 0; i < llavesEncriptacion.length; i++){
-        if(textoCapturado.includes(llavesEncriptacion[i][0])){
+    for(i = 0; i < llavesEncriptacion.length; i++) {
+        if(textoCapturado.includes(llavesEncriptacion[i][0])) {
             textoCapturado = textoCapturado.replaceAll(llavesEncriptacion[i][0], llavesEncriptacion[i][1]);
         }
     }
@@ -50,21 +58,33 @@ function copiar() {
 }
 
 //Desencriptación
-function resultadoDesencriptar(){
+function resultadoDesencriptar() {
     var textoDesencriptado = desencriptar(inputText.value);
 
-    //muestra el resultado del texto ya desencriptado
-    outputText.value = textoDesencriptado;
-    //vaciar textarea del bloque izquierdo
-    inputText.value = "";
+    if(inputText.value.length == 0) {
+        //muestra la instrucción 
+        instruccion.style.display = "inline-flex";
+        //oculta el resultado
+        mensaje.style.display = "none";
+        inputText.focus();
+    } else {
+        //muestra el resultado del texto ya desencriptado
+        outputText.value = textoDesencriptado;
+        //vaciar textarea del bloque izquierdo
+        inputText.value = "";
+        //oculta la instrucción 
+        instruccion.style.display = "none";
+        //muestra el resultado
+        mensaje.style.display = "inline-flex";
+    }
 } 
 
-function desencriptar(textoCopiado){
+function desencriptar(textoCopiado) {
     let llavesEncriptacion = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
     
     //desencriptación
-    for(i=0; i < llavesEncriptacion.length; i++){
-        if (textoCopiado.includes(llavesEncriptacion[i][1])){
+    for(i=0; i < llavesEncriptacion.length; i++) {
+        if(textoCopiado.includes(llavesEncriptacion[i][1])) {
             textoCopiado = textoCopiado.replaceAll(llavesEncriptacion[i][1], llavesEncriptacion[i][0]);
         }
     }
